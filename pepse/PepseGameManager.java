2fda564e418cd51079ea15fbecd5196d2c8fa457
inbaravigdor <inbar.avigdor@mail.huji.ac.il>
2022-12-30 20:02:46 +0200

    added Night, Sun

--- pepse/PepseGameManager.java
+++ pepse/PepseGameManager.java
@@ -1,17 +1,26 @@
 package pepse.util.pepse;
 
 import danogl.GameManager;
+import danogl.collisions.Layer;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import pepse.util.pepse.world.daynight.Night;
+import pepse.util.pepse.world.daynight.Sun;
 
 public class PepseGameManager extends GameManager {
+    private static final float DAY_CYCLE_LENGTH = 30f;
+
     //override initializeGame method in gamemanger
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader,
                                UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        Night.create(gameObjects(), Layer.FOREGROUND, windowController.getWindowDimensions(),
+                DAY_CYCLE_LENGTH);
+        Sun.create(gameObjects(), Layer.BACKGROUND + 1, windowController.getWindowDimensions(),
+                DAY_CYCLE_LENGTH);
     }
     //main function
     public static void main(String[] args) {
--- pepse/world/daynight/Sun.java
+++ pepse/world/daynight/Sun.java
@@ -1,4 +1,31 @@
 package pepse.util.pepse.world.daynight;
 
+import danogl.GameObject;
+import danogl.collisions.GameObjectCollection;
+import danogl.collisions.Layer;
+import danogl.components.CoordinateSpace;
+import danogl.components.Transition;
+import danogl.gui.rendering.OvalRenderable;
+import danogl.util.Vector2;
+
+import java.awt.*;
+
 public class Sun {
+    private static final Vector2 SUN_DIMENSIONS = new Vector2(100, 100);
+    private static final String SUN_TAG = "sun";
+
+    public static GameObject create(GameObjectCollection gameObjects, int layer,
+                                    Vector2 windowDimensions, float cycleLength) {
+        GameObject sun = new GameObject(Vector2.ZERO, SUN_DIMENSIONS, new OvalRenderable(Color.YELLOW));
+        sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        sun.setTag(SUN_TAG);
+
+        // TODO
+
+        gameObjects.addGameObject(sun, layer);
+        return sun;
+
+
+
+    }
 }

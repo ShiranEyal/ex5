c169bec71fa9af73b1dce1f8b46eb1d47fe3f4e7
shiran eyal <shiran.eyal@gmail.com>
2022-12-30 19:58:33 +0200

    finished start of terrain

--- pepse/PepseGameManager.java
+++ pepse/PepseGameManager.java
@@ -1,19 +1,46 @@
 package pepse.util.pepse;
 
 import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
 import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import danogl.util.Vector2;
+import pepse.util.pepse.world.Sky;
+import pepse.util.pepse.world.Terrain;
 
 public class PepseGameManager extends GameManager {
-    //override initializeGame method in gamemanger
+    /**
+     * override initializeGame method in gamemanger
+     * @param imageReader Contains a single method: readImage, which reads an image from disk.
+     *                 See its documentation for help.
+     * @param soundReader Contains a single method: readSound, which reads a wav file from
+     *                    disk. See its documentation for help.
+     * @param inputListener Contains a single method: isKeyPressed, which returns whether
+     *                      a given key is currently pressed by the user or not. See its
+     *                      documentation.
+     * @param windowController Contains an array of helpful, self explanatory methods
+     *                         concerning the window.
+     */
     @Override
     public void initializeGame(ImageReader imageReader, SoundReader soundReader,
                                UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        //create sky
+        Sky sky = new Sky();
+        Vector2 windowSize = windowController.getWindowDimensions();
+        sky.create(this.gameObjects(), windowSize, Layer.BACKGROUND);
+        //create ground blocks
+        Terrain T = new Terrain(this.gameObjects(), Layer.DEFAULT, windowSize, 0);
+        T.createInRange(0, (int) windowSize.x());
     }
-    //main function
+
+    /**
+     * main function
+     * @param args
+     */
     public static void main(String[] args) {
         new PepseGameManager().run();
     }
